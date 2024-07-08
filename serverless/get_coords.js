b31f29e34950106df445aa42de7b94e3d61820f1
LeoKorpi <leokorpi.game@gmail.com>
2024-07-08 13:11:21 +0200

    added serverless functions

---
+++ serverless/get_coords.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+const { WEATHER_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+  const params = JSON.parse(event.body);
+  const { text, units } = params;
+  const regex = /^\fd+$/g;
+  const flag = regex.test(text) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&APPID=${WEATHER_API_KEY}&units=${units}`;
+  const encodedUrl = encodeURI(url);
+  try {
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonData),
+    };
+  } catch (err) {
+    return { statusCode: 422, body: err.stack };
+  }
+};
